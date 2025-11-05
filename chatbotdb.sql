ca00b7ef55b1dee2151f3bc8d83564e13b22bbe5
HECS-CS <haziel.san0201@gmail.com>
2025-11-05 14:26:55 -0600

    Add files via upload

---
+++ chatbotdb.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-11-2025 a las 21:22:57
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `chatbotdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admins`
+--
+-- Error leyendo la estructura de la tabla chatbotdb.admins: #1932 - Table &#039;chatbotdb.admins&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla chatbotdb.admins: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `chatbotdb`.`admins`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestas`
+--
+-- Error leyendo la estructura de la tabla chatbotdb.respuestas: #1932 - Table &#039;chatbotdb.respuestas&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla chatbotdb.respuestas: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `chatbotdb`.`respuestas`&#039; en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
